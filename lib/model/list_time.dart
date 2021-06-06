ce827dfad797deb66aec7137bbf9011b8fe225aa
wyznu <wiz51ceae@gmail.com>
2021-06-07 05:15:29 +0700

    update format

--- lib/model/list_time.dart
+++ lib/model/list_time.dart
@@ -1,16 +1,17 @@
-
 import 'fungsi.dart';
 
-class ListTime extends Fungsi{ // mencari selisih waktu (dlm thn, bln, dan hari)
+class ListTime extends Fungsi {
+  // mencari selisih waktu (dlm thn, bln, dan hari)
 
-    static List <int> lengkap(int tgls, int blns, int thns, int tgl, int bln, int thn) {
-    var hasil = <int>[0,0,0];
+  static List<int> lengkap(
+      int tgls, int blns, int thns, int tgl, int bln, int thn) {
+    var hasil = <int>[0, 0, 0];
     var hTgl = 0;
-      var hBln = 0;
-      var hThn = 0;
+    var hBln = 0;
+    var hThn = 0;
     if (tgls < tgl) {
       var J = Fungsi.fun(blns - 1, thns);
-      
+
       hTgl = tgls + J - tgl;
       blns = blns - 1;
       if (blns < bln) {
@@ -46,7 +47,4 @@ class ListTime extends Fungsi{ // mencari selisih waktu (dlm thn, bln, dan hari)
     hasil[2] = hThn;
     return hasil;
   }
-
 }
-
-
